db390da1819b7692bc09a867784edda1e304e558
Ryan Sechrest <ryan@sechrest.me>
2024-05-04 23:43:08 -0400

    Increment version from 1.0.4 to 1.0.5

--- github-updater-demo.php
+++ github-updater-demo.php
@@ -5,7 +5,7 @@ namespace RYSE\GitHubUpdaterDemo;
 /**
  * Plugin Name:        GitHub Updater Demo
  * Plugin URI:         https://ryansechrest.github.io/github-updater-demo
- * Version:            1.0.4
+ * Version:            1.0.5
  * Description:        WordPress plugin to demonstrate how `GitHubUpdater` can enable WordPress to check for and update a custom plugin that's hosted in either a public or private repository on GitHub.
  * Author:             Ryan Sechrest
  * Author URI:         https://ryansechrest.com/
