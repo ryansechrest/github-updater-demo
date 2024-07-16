b50d0b6b00c64cf35bd9f2f33bc2c3a02705509b
Ryan Sechrest <ryan@sechrest.me>
2024-07-16 16:48:41 -0400

    Add `Tested up to` plugin header field

--- github-updater-demo.php
+++ github-updater-demo.php
@@ -5,11 +5,12 @@ namespace RYSE\GitHubUpdaterDemo;
 /**
  * Plugin Name:        GitHub Updater Demo
  * Plugin URI:         https://ryansechrest.github.io/github-updater-demo
- * Version:            1.0.5
+ * Version:            1.0.6
  * Description:        WordPress plugin to demonstrate how `GitHubUpdater` can enable WordPress to check for and update a custom plugin that's hosted in either a public or private repository on GitHub.
  * Author:             Ryan Sechrest
  * Author URI:         https://ryansechrest.com/
  * Text Domain:        ryse-github-updater-demo
+ * Tested up to:       6.6
  * Requires at least:  6.5
  * Requires PHP:       8.2
  * Update URI:         https://github.com/ryansechrest/github-updater-demo
